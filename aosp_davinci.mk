1cc92f0f5e90d534d4eb13551c6cfa1de09c5a0c
Karan Parashar <whyredfire@gmail.com>
2021-12-31 19:09:28 +0530

    davinci: Define bootanimation resolution

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -14,6 +14,7 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 
 # Inherit some common PixelOS stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_NAME := aosp_davinci
 PRODUCT_DEVICE := davinci
