e2b61a79e291bf28f153d7cb5271e23ae85ecf6a
basamaryan <basam.aryan@gmail.com>
2022-07-12 08:01:43 +0530

    davinci: Welcome PixelOS
    Change-Id: I1fd336ca0c45ac6d2de8444f024b1e18e955df21

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2022 PixelOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,10 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_davinci
+PRODUCT_NAME := aosp_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
