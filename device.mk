b6d97a42d785718c03cc6b52c45bec4fdb19cda5
basamaryan <basam.aryan@gmail.com>
2022-07-13 05:43:03 +0530

    davinci: Include GrapheneCamera
    Change-Id: I031f64bc14809f1682ec1163ff5a8e663b6d9cc2

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    GrapheneCamera
+
 # Camera motor
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.motor@1.0.vendor \
